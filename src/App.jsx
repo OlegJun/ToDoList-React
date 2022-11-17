57b5a556b3b6db77fa6bd561cfa9f50c123a1039
OlegJun <olegpotsah290@gmail.com>
2022-11-17 17:03:40 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Modules/Footer";
+import Main from "./Modules/Main";
+import Header from "./Modules/Header";
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
